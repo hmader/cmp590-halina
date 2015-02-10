0335c7d288c05d1984d75ded2bac711b9e45b5c2
Halina Mader <h.mader@umiami.edu>
2015-02-10 01:22:37 -0500

    First upload of Responsive recipe

---
+++ Responsive_Recipe/js/main.js
@@ -0,0 +1,7 @@
+
+
+$(window).scroll(function(i){
+    var scrollVar = $(window).scrollTop();
+    $("div.first").children("layer-top").css({'top': .7*scrollVar });
+    $("div.first").children("layer-top").css({'opacity':( 100-scrollVar )/100});
+})
