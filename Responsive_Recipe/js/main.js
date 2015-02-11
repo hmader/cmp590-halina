8f31271f93358d8384a4d0434b047e1f33cb6e82
Halina Mader <h.mader@umiami.edu>
2015-02-10 23:27:00 -0500

    Added version with scripts of Responsive recipe

--- Responsive_Recipe/js/main.js
+++ Responsive_Recipe/js/main.js
@@ -4,4 +4,5 @@ $(window).scroll(function(i){
     var scrollVar = $(window).scrollTop();
     $("div.first").children("layer-top").css({'top': .7*scrollVar });
     $("div.first").children("layer-top").css({'opacity':( 100-scrollVar )/100});
+    
 })
