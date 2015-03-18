d738fc1e0989117a9c2999bb76f0fb0161c6126f
Halina Mader <h.mader@umiami.edu>
2015-03-17 22:56:55 -0400

    added working map

---
+++ Map_Jquery/js/myMap.js
@@ -0,0 +1,717 @@
+var Stops = [
+    {
+        lat: 44.32,
+        lng: -69.77,
+        title: "Augusta",
+        html: "<h3>Augusta</h3>",
+        icon: "http://maps.google.com/mapfiles/markerA.png"
+
+    },
+    {
+        lat: 44.25,
+        lng: -72.57,
+        title: "Montpelier",
+        html: "<h3>Montpelier</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 43.21,
+        lng: -71.54,
+        title: "Concord",
+        html: "<h3>Concord</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 42.36,
+        lng: -71.06,
+        title: "Boston",
+        html: "<h3>Boston</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 41.48,
+        lng: -71.53,
+        title: "Kingston",
+        html: "<h3>Kingston</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 41.31,
+        lng: -72.92,
+        title: "New Haven",
+        html: "<h3>New Haven</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 41.76,
+        lng: -72.67,
+        title: "Hartford",
+        html: "<h3>Hartford</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 42.65,
+        lng: -73.76,
+        title: "Albany",
+        html: "<h3>Albany</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 40.71,
+        lng: -74.01,
+        title: "New York City",
+        html: "<h3>New York City</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 40.22,
+        lng: -74.76,
+        title: "Trenton",
+        html: "<h3>Trenton</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 39.95,
+        lng: -75.17,
+        title: "Philadelphia",
+        html: "<h3>Philadelphia</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 39.75,
+        lng: -75.55,
+        title: "Wilmington",
+        html: "<h3>Wilmington</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 39.98,
+        lng: -82.98,
+        title: "Columbus",
+        html: "<h3>Columbus</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 42.33,
+        lng: -83.05,
+        title: "Detroit",
+        html: "<h3>Detroit</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 42.73,
+        lng: -84.55,
+        title: "Lansing",
+        html: "<h3>Lansing</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 39.79,
+        lng: -86.15,
+        title: "Indianapolis",
+        html: "<h3>Indianapolis</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 41.83,
+        lng: -87.69,
+        title: "Chicago",
+        html: "<h3>Chicago</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 43.05,
+        lng: -87.95,
+        title: "Milwaukee",
+        html: "<h3>Milwaukee</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 43.07,
+        lng: -89.40,
+        title: "Madison",
+        html: "<h3>Madison</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 44.98,
+        lng: -93.27,
+        title: "Minneapolis",
+        html: "<h3>Minneapolis</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 46.88,
+        lng: -96.79,
+        title: "Fargo",
+        html: "<h3>Fargo</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 43.54,
+        lng: -96.73,
+        title: "Sioux Falls",
+        html: "<h3>Sioux Falls</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 44.28,
+        lng: -105.51,
+        title: "Gillette",
+        html: "<h3>Gillette</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 46.56,
+        lng: -112.03,
+        title: "Helena",
+        html: "<h3>Helena</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 43.62,
+        lng: -116.20,
+        title: "Boise",
+        html: "<h3>Boise</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 47.66,
+        lng: -117.43,
+        title: "Spokane",
+        html: "<h3>Spokane</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 47.61,
+        lng: -122.33,
+        title: "Seattle",
+        html: "<h3>Seattle</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 45.52,
+        lng: -122.68,
+        title: "Portland",
+        html: "<h3>Portland</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 40.58,
+        lng: -122.37,
+        title: "Redding",
+        html: "<h3>Redding</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 39.53,
+        lng: -119.82,
+        title: "Reno",
+        html: "<h3>Reno</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 37.36,
+        lng: -118.40,
+        title: "Bishop",
+        html: "<h3>Bishop</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    },
+    {
+        lat: 34.05,
+        lng: -118.25,
+        title: "Los Angeles",
+        html: "<h3>Los Angeles</h3>",
+        icon: "http://maps.google.com/mapfiles/marker.png"
+
+    }
+];
+
+/* ==============================================================================
+=============================================================================== */
+
+var waypts1 = [
+    {
+        location: new google.maps.LatLng(44.25, -72.57),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.21, -71.54),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.36, -71.06),
+        stopover: true
+    },
+    {
+        location: new google.maps.LatLng(41.48, -71.53),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.31, -72.92),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.76, -72.67),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.65, -73.76),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(40.71, -74.01),
+        stopover: true
+
+    }];
+
+waypts2 = [
+    {
+        location: new google.maps.LatLng(39.95, -75.17),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.75, -75.55),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.98, -82.98),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.33, -83.05),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.73, -84.55),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.79, -86.15),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.83, -87.69),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.05, -87.95),
+        stopover: true
+
+    }];
+
+waypts3 = [
+    {
+        location: new google.maps.LatLng(44.98, -93.27),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(46.88, -96.79),
+        stopover: true
+    },
+    {
+        location: new google.maps.LatLng(43.54, -96.73),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(44.28, -105.51),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(46.56, -112.03),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.62, -116.20),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(47.66, -117.43),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(47.61, -122.33),
+        stopover: true
+
+    }];
+
+waypts4 = [
+    {
+        location: new google.maps.LatLng(40.58, -122.37),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.53, -119.82),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(37.36, -118.40),
+        stopover: true
+
+    }
+];
+
+/* ==============================================================================
+=============================================================================== */
+
+var styles = [
+    {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+            {
+                hue: "#00ffe6"
+            },
+            {
+                saturation: -20
+            }
+              ]
+            },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#e9ebed"
+            },
+            {
+                "saturation": -78
+            },
+            {
+                "lightness": 67
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+            {
+                lightness: 100
+            },
+            {
+                visibility: "simplified"
+            }
+              ]
+            }, {
+        elementType: "labels",
+        stylers: [
+            {
+                visibility: "off"
+            }
+              ]
+            }, {
+        featureType: "administrative.province",
+        elementType: "labels",
+        stylers: [
+            {
+                visibility: "on"
+            }
+              ]
+            }
+        ];
+
+/* ==============================================================================
+=============================================================================== */
+/*
+var Waypoints = [];
+
+$(document).ready(function(){
+    
+    $.each(Stops, function(i, marker) {
+        var latlng = new google.maps.LatLng(marker.lat, marker.lng);
+        Waypoints.push({
+            location: latlng,
+            stopover: true});
+    });
+  */
+/* ==============================================================================
+=============================================================================== */
+
+var map = new GMaps({
+    el: '#map',
+    lat: 39.8282,
+    lng: -98.5795,
+    zoom: 4,
+    draggable: false,
+    scrollwheel: false,
+    zoomControl: false,
+    panControl: false,
+    streetViewControl: false,
+    mapTypeControl: false
+});
+
+map.drawRoute({
+    origin: [44.3235, -69.7653],
+    destination: [40.22, -74.76],
+    waypoints: waypts1,
+    travelMode: "driving",
+    strokeColor: '#1975A3',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+
+});
+
+map.drawRoute({
+ origin: [40.22, -74.76],
+    destination: [43.07, -89.40],
+    waypoints: waypts2,
+    travelMode: "driving",
+    strokeColor: '#1975A3',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+});
+
+map.drawRoute({
+ origin: [43.07, -89.40],
+    destination: [45.52, -122.68],
+    waypoints: waypts3,
+    travelMode: "driving",
+    strokeColor: '#1975A3',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+});
+
+map.drawRoute({
+ origin: [45.52, -122.68],
+    destination: [34.05, -118.25],
+    waypoints: waypts4,
+    travelMode: "driving",
+    strokeColor: '#1975A3',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+});
+
+
+/*
+map.drawPolyline({
+    path: Stops,
+    strokeColor: '#1975A3',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+});
+*/
+map.addStyle({
+    styledMapName: "Styled Map",
+    styles: styles,
+    mapTypeId: "map_style"
+});
+
+map.setStyle("map_style");
+
+map.addMarkers(Stops);
+//  });
+
+/*
+var Waypoints = [
+
+    {
+        location: new google.maps.LatLng(44.25, -72.57),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.21, -71.54),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.36, -71.06),
+        stopover: true
+    },
+    {
+        location: new google.maps.LatLng(41.48, -71.53),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.31, -72.92),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.76, -72.67),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.65, -73.76),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(40.71, -74.01),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(40.22, -74.76),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.95, -75.17),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.75, -75.55),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.98, -82.98),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.33, -83.05),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(42.73, -84.55),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.79, -86.15),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(41.83, -87.69),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.05, -87.95),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.07, -89.40),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(44.98, -93.27),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(46.88, -96.79),
+        stopover: true
+    },
+    {
+        location: new google.maps.LatLng(43.54, -96.73),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(44.28, -105.51),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(46.56, -112.03),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(43.62, -116.20),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(47.66, -117.43),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(47.61, -122.33),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(45.52, -122.68),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(40.58, -122.37),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(39.53, -119.82),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(37.36, -118.40),
+        stopover: true
+
+    },
+    {
+        location: new google.maps.LatLng(34.05, -118.25),
+        stopover: true
+
+    }
+];
+
+*/
